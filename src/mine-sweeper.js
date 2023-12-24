8bbb35e396c7eb6a38e63c858f426ec2ce31a302
Igor Checheurov <chei@yandex.ru>
2023-12-24 19:06:34 +0300

    feat: add more solutions

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let fieldWidth = matrix[0].length;
+  let filedHeight = matrix.length;
+  let result = [];
+  for (let i = 0; i < filedHeight; i++) {
+    result[i].fill(0, fieldWidth);
+  }
+
+  for (let i = 0; i < fieldWidth; i++) {
+    for (let j = 0; j < filedHeight; j++) {
+
+      if (matrix[i][j] == true) {
+        if (i > 0 && i < fieldWidth - 1) {
+          result[i - 1][j] += 1;
+          if (j > 0) result[i - 1][j - 1] += 1;
+          if (j < filedHeight - 1) result[i - 1][j + 1] += 1;
+        }
+
+
+        if 
+      }
+      
+      // if (i > 0 && j > 0) {
+      //   if (matrix[i - 1][j - 1] == true) result[i][j] += 1;
+      //   if (matrix[i - 1][j] == true) result[i][j] += 1;
+      //   if (matrix[i][j - 1] == true) result[i][j] += 1;
+      // }
+
+      // if (i < filedHeight - 1 && j > 0) {
+      //   if (matrix[i + 1][j - 1] == true) result[i][j] += 1;
+      //   if (matrix[i + 1][j] == true) result[i][j] += 1;
+      // }
+      
+      // if (i < filedHeight - 1 && j > 0) {
+      //   if (matrix[i + 1][j - 1] == true) result[i][j] += 1;
+      //   if (matrix[i + 1][j] == true) result[i][j] += 1;
+      // }
+
+      
+
+
+      checkField(i, j);
+    }
+  }
+
+  function checkField(i, j) {
+    let limitLeft = i > 0 ? i - 1 : false;
+    let limitRight = i == fieldWidth - 1 ? false : i + 1;
+    let limitTop = j > 0 ? j - 1 : false;
+    let limitBottom = j == filedHeight - 1 ? false : j + 1;
+
+    for ()
+
+  }
 }
 
 module.exports = {
